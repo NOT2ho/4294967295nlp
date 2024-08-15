c810edb30dfedf106919201eaea7de894e4060a4
4294967295 <4294967295@snu.ac.kr>
2024-08-15 21:22:32 +0900

    initial commit

---
+++ markov.py
@@ -0,0 +1,48 @@
+import bisect
+import itertools
+import random
+import re
+
+import nltk
+from konlpy.tag import Mecab 
+mecab=Mecab()
+
+def generate_sentence(cfdist, word):
+    sentence = []
+
+    while word!='.':
+        sentence.append(word)
+
+        choices, weights = zip(*cfdist[word].items())
+        cumdist = list(itertools.accumulate(weights))
+        x = random.random() * cumdist[-1]
+        word = choices[bisect.bisect(cumdist, x)]
+
+    regEx = re.compile('^J|^E|^X|^S')
+    res = ''
+    for i in sentence:
+        tp = mecab.pos(i[0])
+        tag = tp[0][1]
+        if regEx.match(tag)==None:
+            res += ' '+i
+
+        else:
+            res += i       
+
+    return res
+
+
+def calc_cfd(doc):
+    words = [w for w, t in Mecab().pos(doc)]
+    ngrams = nltk.ngrams(words, n=2)
+    #condition_pairs = (((w0, w1), w2) for w0, w1, w2 in ngrams)
+    return nltk.ConditionalFreqDist(ngrams)
+
+
+if __name__=='__main__':
+    initstr = u'나' 
+
+    doc = open('4294967295.txt', 'rt', encoding='UTF8').read()
+    cfd = calc_cfd(doc)
+
+    print(generate_sentence(cfd, initstr))
