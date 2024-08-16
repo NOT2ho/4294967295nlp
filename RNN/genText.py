10de58c4f2677071119d27b5738868cdd462c10d
4294967295 <4294967295@snu.ac.kr>
2024-08-16 15:42:03 +0900

    attach ^J|^E|^X|^S

--- RNN/genText.py
+++ RNN/genText.py
@@ -1,7 +1,8 @@
 # coding: utf-8
-
+import re
 from konlpy.tag import Mecab 
 import sys
+mecab=Mecab()
 sys.path.append('.')
 sys.path.append('..')
 sys.path.append('../../')
@@ -25,7 +26,17 @@ skip_words = ['는']
 skip_ids = [word_to_id[w] for w in skip_words]
 
 word_ids = model.generate(start_id, skip_ids)
-txt = ' '.join([id_to_word[i] for i in word_ids])
+# txt = ' '.join([id_to_word[i] for i in word_ids])
 #txt = txt.replace('', '')
+regEx = re.compile('^J|^E|^X|^S')
+res = ''
+for i in [id_to_word[i] for i in word_ids]:
+    tp = mecab.pos(i[0])
+    tag = tp[0][1]
+    if regEx.match(tag)==None:
+        res += ' '+i
 
-print(txt)
+    else:
+        res += i       
+
+print(res)
