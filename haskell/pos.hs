bd60a2b44961569adbfd066dc67826ff698b3743
not2ho <4294967295@snu.ac,kr>
2024-09-02 14:22:29 +0900

    had lunch

--- haskell/pos.hs
+++ haskell/pos.hs
@@ -5,7 +5,7 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Sequence
 
-data Trie = Node (Map Char (Bool, Output, Trie))
+data Trie = Node (Map Char (Bool, Output, Trie, Trie))
             | Null
             deriving (Show)
 
@@ -17,8 +17,8 @@ member :: Char -> Map Char a -> Bool
 member = Map.member
 notMember = Map.notMember
 ins = Map.insert
-fwd :: Char -> Map Char (Bool, Output, Trie) -> (Bool, Output, Trie)
-fwd = Map.findWithDefault (False, NA, Null)
+fwd :: Char -> Map Char (Bool, Output, Trie, Trie) -> (Bool, Output, Trie, Trie)
+fwd = Map.findWithDefault (False, NA, Null, Null)
 lnull = Data.List.null
 llength :: [] a -> Int
 llength = Data.List.length
@@ -28,17 +28,19 @@ elems = Map.elems
 trieInsert :: [Char] -> Output -> Trie -> Trie
 trieInsert [] cl t = t
 trieInsert (w:ws) cl Null 
-    | lnull ws = Node $ st w (True, cl, Null)
-    | otherwise = Node $ st w (False, NA, trieInsert ws cl Null)
+    | lnull ws = Node $ st w (True, cl, Null, Null)
+    | otherwise = Node $ st w (False, NA, trieInsert ws cl Null, Null)
 trieInsert (w:ws) cl (Node m)
     | member w m=
-        let (b,o,t) = fwd w m in
+        let (b,o,t, f) = fwd w m in
         Node $ ins w (lnull ws || b, 
                         if lnull ws || b then o else NA,
-                        trieInsert ws cl t) m
+                        trieInsert ws cl t,
+                        f) m
     | notMember w m = Node $ ins w (lnull ws, 
                                      if lnull ws then cl else NA, 
-                                   trieInsert ws cl Null) m
+                                   trieInsert ws cl Null,
+                                   Null) m
 
 acInsert :: Output -> Trie -> Trie
 acInsert (Pos (word, pos)) = trieInsert word (Pos (word, pos) )
@@ -47,20 +49,26 @@ acInsert (Pos (word, pos)) = trieInsert word (Pos (word, pos) )
 acGo :: Char -> Trie -> Trie
 acGo c (Node m)
     | member c m = 
-        let (b,o,t) = fwd c m in
+        let (b,o,t, f) = fwd c m in
             t
     | otherwise = Null
 acGo c Null = Null
+
 
 acSearch :: [Char] -> Trie -> Maybe (Map Int Output)
 acSearch (s:ss) (Node m) = do
     let nextNode = acGo s (Node m) 
     case nextNode of
-        Null -> acSearch ss (acFail $ Node m)
+        Null -> let (b,Pos(word, pos),nextTrie, f) = fwd s m in
+            acSearch ss f
         _ -> 
-            let (b,Pos(word, pos),nextTrie) = fwd s m in
+            let (b,Pos(word, pos),nextTrie, f) = fwd s m in
             if not b then acSearch ss nextTrie else Just $ st (llength word) $ Pos(word, pos)
 acSearch s Null = Nothing
 
-acFail :: Trie -> Trie
-acFail (Node m) = Null
+acFail :: [Char] -> Trie -> Trie
+acFail str Null = Null
+acFail (x:xs) (Node m) = do
+    let que = empty 
+    let (b,o,t,f) = fwd x m in
+        t
