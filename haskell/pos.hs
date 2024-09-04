615e313419bc4829589aaacb38886e2acc4e207c
not2ho <4294967295@snu.ac,kr>
2024-09-04 19:34:17 +0900

    made trie better again

--- haskell/pos.hs
+++ haskell/pos.hs
@@ -6,7 +6,7 @@ import qualified Data.Map as Map
 import Data.Sequence
 
 data Trie = Root Childs
-            | Node Char Bool (Maybe Output) Fail Childs
+            | Node Bool (Maybe Output) Fail Childs
             | Null
             deriving (Show)
 type Fail = Trie
@@ -16,78 +16,98 @@ type Pos = [Char]
 
 st = Map.singleton
 member = Map.member
+notMember :: Char -> Map Char a -> Bool
 notMember = Map.notMember
 ins :: Char -> a -> Map Char a -> Map Char a
 ins = Map.insert
-fwd = Map.findWithDefault Null
+fwd :: Ord k => a -> k -> Map k a -> a
+fwd = Map.findWithDefault
 lnull = Data.List.null
 llength :: [] a -> Int
 llength = Data.List.length
 keys = Map.keys
 elems = Map.elems
 qct = Data.Sequence.singleton
+stake :: Int -> Seq Char -> Seq Char
 stake = Data.Sequence.take
+--madjust = Map.adjust
+
 
 trieInsert :: [Char] -> Maybe Output -> Trie -> Trie
-trieInsert [] _ t = t
+trieInsert [] output t = 
+    case t of
+        Null -> Null --이런 일은 없어야 함
+        Root m -> t --이런 일은 없어야 함
+        Node b o f m -> Node True output f m 
+
+
 trieInsert (w:ws) output Null
-    | lnull ws =  Node w True output Null Map.empty
-    | otherwise = Node w False Nothing Null $ st (head ws) $ trieInsert ws output Null
+    | lnull (w:ws) = Null
+    | lnull ws =  Node True output Null (st w Null)
+    | otherwise = Node False Nothing Null $ st w $ trieInsert ws output Null
 
 trieInsert (w:ws) output (Root m)
-    | Map.null m =
-        Root $ st w $ trieInsert (w:ws) output Null
     | member w m =
-        let cm = fwd w m in
-        let newCm = trieInsert ws output cm in
-        Root (ins w newCm m)
+        let nextNode = fwd Null w m in 
+        let thisNode = trieInsert ws output nextNode in 
+        Root (ins w thisNode m)
     | notMember w m =
-        let cm = Node w (lnull ws) (if lnull ws then output else Nothing) Null Map.empty in
-        let newCm = trieInsert ws output cm in
-        Root (ins w newCm m)
-trieInsert (w:ws) output (Node c b o f m)
-    | Map.null m =
-        Node c b o f $ st w $ trieInsert (w:ws) output Null
+        let nextNode = Node (lnull ws)
+                                (if lnull ws then output else Nothing) 
+                                Null 
+                                Map.empty in
+        let thisNode = trieInsert ws output nextNode in
+        Root (ins w thisNode m)
+trieInsert (w:ws) output (Node b o f m)
     | member w m =
-        let cm = fwd w m in
-        let newCm = trieInsert ws output cm in
-        Node c b o f (ins w newCm m)
+        let nextNode = fwd Null w m in
+        let thisNode = trieInsert ws output nextNode in
+        Node b o f (ins w thisNode m)
     | notMember w m =
-        let cm = Node w (lnull ws) (if lnull ws then output else Nothing) Null Map.empty in
-        let newCm = trieInsert ws output cm in
-        Node c b o f (ins w newCm m)
+        let nextNode = Node (lnull ws) 
+                                (if lnull ws then output else Nothing) 
+                                Null 
+                                Map.empty in
+        let thisNode = trieInsert ws output nextNode in
+        Node b o f (ins w thisNode m)
+
 
 
 acInsert :: Output -> Trie -> Trie
 acInsert (word, pos) = trieInsert word (Just (word, pos))
 
-
+{-
 acGo :: Char -> Trie -> Trie
-acGo char (Node c b o t m)
+acGo char (Node b o t m)
     | member c m =
-        let (b,o,t, f) = fwd char m in
+        let node = fwd Null char m in
             t
     | otherwise = Null
 acGo c Null = Null
-
-
+-}
+{-
 acSearch :: [Char] -> Trie -> Maybe (Map Int Output)
-acSearch (s:ss) (Node c b o t m) = do
-    let nextNode = acGo s (Node c b o t m)
-    let (b,Output(word, pos),nextTrie, f) = fwd s m
-    case nextNode of
+acSearch (s:ss) (Node c b o f m) = do
+    let thisNode = acGo s (Node c b o f m)
+    let nextTrie = fwd s m
+    case thisNode of
         Null -> acSearch ss f
         _ -> if not b then
             acSearch ss nextTrie
-            else Just $ st (llength word) $ Output (word, pos)
-acSearch s Null = Nothing
-
-{-
-acFail ::  Trie -> Trie
-acFail Null = Null
-acFail Root = do
-    let (b, Just (w,p),t,f) = fwd x m
-    let emptyQue = empty
-    let que = emptyQue >< qct (keys m)
-
+            --else Just $ st (llength word) $ Just (word, pos)
+--acSearch s Null = Nothing
 -}
+{-
+acFail :: Trie -> Trie
+acFail (Root child) =
+        let (h:t) = keys child in
+            let Node b o f m = fwd Null h child in
+                case h:t of
+                    [_] ->
+                        Root
+                            $ madjust (const $ Node c b o (Root child) m) h child
+                    _ ->
+                            let Root last = acFail $ Root (Map.delete h child) in
+                                Root
+                                     $ madjust (const $ Node c b o (Root child) m) h last
+--acFail (Node c b o f m) =-}
