2eecbd8625ef7b7419272ebe888cde8f925f651e
not2ho <4294967295@snu.ac,kr>
2024-09-05 15:00:08 +0900

    ac not done

--- haskell/pos.hs
+++ haskell/pos.hs
@@ -4,11 +4,13 @@ import Data.Map.Internal.Debug
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Sequence
+import Data.Maybe(fromMaybe)
 
 data Trie = Root Childs
             | Node Bool (Maybe Output) Fail Childs
             | Null
-            deriving (Show)
+            | IfYouSeeThisThenYouArePoorCat
+            deriving (Show, Eq)
 type Fail = Trie
 type Childs = Map Char Trie
 type Output = ([Char], Pos)
@@ -31,14 +33,15 @@ qct = Data.Sequence.singleton
 stake :: Int -> Seq Char -> Seq Char
 stake = Data.Sequence.take
 --madjust = Map.adjust
+sst = Data.Sequence.singleton
 
 
 trieInsert :: [Char] -> Maybe Output -> Trie -> Trie
-trieInsert [] output t = 
+trieInsert [] output t =
     case t of
-        Null -> Null --이런 일은 없어야 함
-        Root m -> t --이런 일은 없어야 함
-        Node b o f m -> Node True output f m 
+        Null -> IfYouSeeThisThenYouArePoorCat
+        Root m -> IfYouSeeThisThenYouArePoorCat
+        Node b o f m -> Node True output f m
 
 
 trieInsert (w:ws) output Null
@@ -48,25 +51,25 @@ trieInsert (w:ws) output Null
 
 trieInsert (w:ws) output (Root m)
     | member w m =
-        let nextNode = fwd Null w m in 
-        let thisNode = trieInsert ws output nextNode in 
+        let nextNode = fwd IfYouSeeThisThenYouArePoorCat w m in
+        let thisNode = trieInsert ws output nextNode in
         Root (ins w thisNode m)
     | notMember w m =
         let nextNode = Node (lnull ws)
-                                (if lnull ws then output else Nothing) 
-                                Null 
+                                (if lnull ws then output else Nothing)
+                                Null
                                 Map.empty in
         let thisNode = trieInsert ws output nextNode in
         Root (ins w thisNode m)
 trieInsert (w:ws) output (Node b o f m)
     | member w m =
-        let nextNode = fwd Null w m in
+        let nextNode = fwd IfYouSeeThisThenYouArePoorCat w m in
         let thisNode = trieInsert ws output nextNode in
         Node b o f (ins w thisNode m)
     | notMember w m =
-        let nextNode = Node (lnull ws) 
-                                (if lnull ws then output else Nothing) 
-                                Null 
+        let nextNode = Node (lnull ws)
+                                (if lnull ws then output else Nothing)
+                                Null
                                 Map.empty in
         let thisNode = trieInsert ws output nextNode in
         Node b o f (ins w thisNode m)
@@ -76,38 +79,57 @@ trieInsert (w:ws) output (Node b o f m)
 acInsert :: Output -> Trie -> Trie
 acInsert (word, pos) = trieInsert word (Just (word, pos))
 
-{-
-acGo :: Char -> Trie -> Trie
-acGo char (Node b o t m)
-    | member c m =
-        let node = fwd Null char m in
-            t
-    | otherwise = Null
-acGo c Null = Null
--}
-{-
+
+acGo :: Char -> Trie -> Maybe Trie
+acGo char (Node b o f m)
+    | member char m =
+        Just $ fwd IfYouSeeThisThenYouArePoorCat char m
+    | otherwise = Nothing
+acGo c root = Just root
+
+
 acSearch :: [Char] -> Trie -> Maybe (Map Int Output)
-acSearch (s:ss) (Node c b o f m) = do
-    let thisNode = acGo s (Node c b o f m)
-    let nextTrie = fwd s m
-    case thisNode of
-        Null -> acSearch ss f
-        _ -> if not b then
-            acSearch ss nextTrie
-            --else Just $ st (llength word) $ Just (word, pos)
---acSearch s Null = Nothing
--}
-{-
-acFail :: Trie -> Trie
-acFail (Root child) =
-        let (h:t) = keys child in
-            let Node b o f m = fwd Null h child in
-                case h:t of
-                    [_] ->
-                        Root
-                            $ madjust (const $ Node c b o (Root child) m) h child
-                    _ ->
-                            let Root last = acFail $ Root (Map.delete h child) in
-                                Root
-                                     $ madjust (const $ Node c b o (Root child) m) h last
---acFail (Node c b o f m) =-}
+acSearch (s:ss) (Node b o f m) = do
+    let thisNode = acGo s (Node b o f m)
+    let nextTrie = fwd IfYouSeeThisThenYouArePoorCat s m
+    (if b 
+        then Just $ st 1 (fromMaybe ("나오면 안되는값임","IfYouSeeThisThenYouArePoorCat") o) 
+        else acSearch ss nextTrie)
+acSearch s Null = Just $ st 666 ("나오면 안되는값임","IfYouSeeThisThenYouArePoorCat")
+
+
+acFail:: Trie -> Seq Trie -> Maybe Trie
+acFail _ Empty = Nothing
+acFail (Root edge) que =
+    let root = Root edge in
+        let (h:t) = keys edge in
+            let Node b o f childEdge = fwd Null h edge in
+                    case h:t of
+                        [_] ->
+                            Nothing
+                        _ -> acFail (
+                                Node b o f
+                                (ins h (Node b o root childEdge) edge))
+                                (que |> Node b o f childEdge)
+
+
+acFail (Node bool output fail edge) (q :<| qs)
+    | q == Node bool output fail edge =
+        let thisNode = Node bool output fail edge in
+            let (h:t) = keys edge in
+                let Node b o f childEdge = fwd IfYouSeeThisThenYouArePoorCat h edge in
+                    case h:t of
+                        [_] ->
+                            Nothing
+                        _ ->
+                            acFail (
+                                Node b o f
+                                (ins h (Node b o
+                                        (fromMaybe IfYouSeeThisThenYouArePoorCat (acGo h fail))
+                                        childEdge)
+                                edge)
+                                )
+                                (qs|> Node b o f childEdge)
+                                
+
+acFail _ _ = Just IfYouSeeThisThenYouArePoorCat
