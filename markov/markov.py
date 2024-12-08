831a676db686659a35ccce1734c33fd772e75cab
4294967295 <4294967295@snu.ac.kr>
2024-12-08 17:45:49 +0900

    update corpus

--- markov/markov.py
+++ markov/markov.py
@@ -10,7 +10,8 @@ mecab=Mecab()
 def generate_sentence(cfdist, word):
     sentence = []
 
-    while word!='.':
+    for i in range (1,3000) :
+#    while word!='.':
         sentence.append(word)
 
         choices, weights = zip(*cfdist[word].items())
@@ -43,5 +44,4 @@ doc = open('corpus/4294967295.txt', 'rt', encoding='UTF8').read()
 if __name__=='__main__':
     initstr = random.choice([w for w, t in Mecab().pos(doc)])
     cfd = calc_cfd(doc)
-
     print(generate_sentence(cfd, initstr))
